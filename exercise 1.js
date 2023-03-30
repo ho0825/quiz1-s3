59e3a7332c75bc8b027e2f4d623c4fc83cb7a951
ho <hozheheng@gmail.com>
2023-03-30 15:25:26 +0800

    change 3

--- exercise 1.js
+++ exercise 1.js
@@ -1,3 +1,19 @@
 console.log("hello world")
-let messages = 123
+let messages = "benr"
 console.log(messages, typeof messages)
+
+messages = 1234
+console.log(messages, typeof messages)
+
+let str = "BENR"
+let num = 3523
+
+let course = str + num
+console.log(course, typeof course)
+
+let str2 = "1.228"
+let num2 = 1
+
+let total = parseFloat(str2) + num2
+let total2 = total.toFixed(9)
+console.log(total2, typeof total2)
