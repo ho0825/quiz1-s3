1c3e42e82915a5b0e3562cd0e4b59250b5a0a26a
ho <hozheheng@gmail.com>
2023-03-30 15:53:00 +0800

    change 4

--- exercise 1.js
+++ exercise 1.js
@@ -1,9 +1,9 @@
 console.log("hello world")
-let messages = "benr"
-console.log(messages, typeof messages)
+let message = "benr"
+console.log(message, typeof message)
 
-messages = 1234
-console.log(messages, typeof messages)
+message = 1234
+console.log(message, typeof message)
 
 let str = "BENR"
 let num = 3523
@@ -15,5 +15,40 @@ let str2 = "1.228"
 let num2 = 1
 
 let total = parseFloat(str2) + num2
-let total2 = total.toFixed(9)
-console.log(total2, typeof total2)
+let total2 = total.toFixed(2)
+console.log(total2, typeof total2 + "\n")
+
+array1 = ["HO", "KHOO", "TAN"]
+console.log(array1[1])
+console.log(array1[2])
+console.log(array1[3] + "\n")
+
+
+let user = {
+    name : "Ho Zhe Heng", 
+    age : 22, 
+    faculty : "FKEKK"
+}
+console.log(user.name)
+console.log(user.age)
+console.log(user.faculty + "\n")
+
+let comments = [
+    {
+        name : "Ho Zhe Heng",
+        date : "3/30/2023",
+        msg : "Very good",
+        like : 1500
+    },
+    {
+        name : "Khoo",
+        date : "3/31/2023",
+        msg : "Very bad",
+        like : 3000
+    }
+]
+
+console.log(comments[0].name)
+console.log(comments[0].date)
+console.log(comments[0].msg)
+console.log(comments[0].like)
